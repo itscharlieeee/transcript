20cf129305430a7a9c13326e1755afc8e2de9384
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-05-26 12:53:49 -0500

    Create video.py

---
+++ pages/video.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def video_to_transcript(video_file):
+    # Step 1: Convert video to audio
+    audio_file = "temp_audio.wav"
+    audio = AudioSegment.from_file(video_file, format="mp4")
+    audio.export(audio_file, format="wav")
+
+st.title("Transcripción de Video")
+
+st.write("Upload a video file and convert it to a transcript.")
+video_file = st.file_uploader("Upload a video file", type=["mp4", "avi", "mkv"])
+if video_file is not None:
+        # Convert video to transcript
+        #transcript = video_to_transcript(video_file)
+        st.write("Cargado")
