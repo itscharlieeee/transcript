315c100012cf33009eb5df0eaa9e28f14745758b
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-05-26 11:57:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 import streamlit as st
 from tempfile import NamedTemporaryFile
 import whisper
+from openai import OpenAI
+
 
 st.title("Reproductor de Audio")
+ke = st.text_input('Ingresa tu Clave')
+os.environ['OPENAI_API_KEY'] = ke
+
 # Cargar archivo de audio
 uploaded_file = st.file_uploader("Cargar archivo de audio", type=["mp3", "wav"])
 if uploaded_file:
         st.audio(uploaded_file, format="audio/mp3")
 
 if uploaded_file is not None:
-    with NamedTemporaryFile(suffix="mp3") as temp:
-        temp.write(uploaded_file.getvalue())
-        temp.seek(0)
-        model = whisper.load_model("base")
-        result = model.transcribe(temp.name)
-        st.write(result["text"])
+    client = OpenAI(api_key=ke)    
+    transcription = client.audio.transcriptions.create(
+       model="whisper-1", 
+       file=uploaded_file
+     )
+     st.writre(transcription.text)
 
           
