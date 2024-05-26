11a85e0c3783cada7b0d30ab8625e9f13ef104d1
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-05-26 11:29:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ st.title("Reproductor de Audio")
 # Cargar archivo de audio
 uploaded_file = st.file_uploader("Cargar archivo de audio", type=["mp3", "wav"])
 if uploaded_file:
-        st.audio(uploaded_file, format="audio/mp3")
+        #st.audio(uploaded_file, format="audio/mp3")
 
         # Convertir el archivo cargado a un objeto AudioSegment
-        audio_data = AudioSegment.from_file(uploaded_file)
+        #audio_data = AudioSegment.from_file(uploaded_file)
 
         # Reproducir el audio
 #st.subheader("Reproducir Audio")
