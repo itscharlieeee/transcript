db767f0598a19dd8ca1e49baec732622d28a873c
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-05-26 11:33:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import streamlit as st
 st.title("Reproductor de Audio")
 # Cargar archivo de audio
 uploaded_file = st.file_uploader("Cargar archivo de audio", type=["mp3", "wav"])
-#if uploaded_file:
-        #st.audio(uploaded_file, format="audio/mp3")
+if uploaded_file:
+        st.audio(uploaded_file, format="audio/mp3",Loop=True)
 
         # Convertir el archivo cargado a un objeto AudioSegment
         #audio_data = AudioSegment.from_file(uploaded_file)
