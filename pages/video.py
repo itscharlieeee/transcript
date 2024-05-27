0b5d7364de5a2394e099794110683cbe516e77e4
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-05-27 08:09:25 -0500

    Update video.py

--- pages/video.py
+++ pages/video.py
@@ -5,7 +5,8 @@ from moviepy.editor import VideoFileClip
 mp3_file = "audio.mp3"
 def video_to_transcript(video_file):
     # Step 1: Convert video to audio
-    audio_clip = video_file.audio
+    video_clip = VideoFileClip((video_file)
+    audio_clip = video_clip.audio
     # Write the audio to a separate file
     audio_clip.write_audiofile(mp3_file)
     # Close the video and audio clips
