0872988e5a8a7e87eb214a86bd96e1239c95e58d
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-05-27 12:00:16 -0500

    Update video.py

--- pages/video.py
+++ pages/video.py
@@ -11,11 +11,11 @@ def video_to_transcript(video_file):
     # Write the audio to a separate file
     audio_clip.write_audiofile(mp3_file) 
     st.audio("audio.mp3", format="audio/mp3")
-    client = OpenAI(api_key=ke)    
-    transcription = client.audio.transcriptions.create(
-       model="whisper-1", 
-       file=audio_clip
-    )
+    #client = OpenAI(api_key=ke)    
+    #transcription = client.audio.transcriptions.create(
+    #   model="whisper-1", 
+    #   file=audio_clip
+    #)
     st.write(transcription.text)
     # Close the video and audio clips
     audio_clip.close()
