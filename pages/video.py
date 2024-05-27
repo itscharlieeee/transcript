948d0611c75830d2b85a3e2835f0287fa8f99764
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-05-27 11:30:35 -0500

    Update video.py

--- pages/video.py
+++ pages/video.py
@@ -5,7 +5,7 @@ from moviepy.editor import VideoFileClip
 mp3_file = "audio.mp3"
 def video_to_transcript(video_file):
     # Step 1: Convert video to audio
-    video_clip = VideoFileClip(Less Sensor.mp4)
+    video_clip = VideoFileClip("Less Sensor.mp4")
     audio_clip = video_clip.audio
     # Write the audio to a separate file
     audio_clip.write_audiofile(mp3_file) 
