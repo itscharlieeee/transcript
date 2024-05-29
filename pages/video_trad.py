3a3d0a6a29d6f62b5b3692bc80d47a85cf3347ee
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-05-29 15:04:45 -0500

    Update video_trad.py

--- pages/video_trad.py
+++ pages/video_trad.py
@@ -14,12 +14,12 @@ mp3_file = "audio.mp3"
 def text_to_speech(input_language, output_language, text, tld):
         translation = translator.translate(text, src=input_language, dest=output_language)
         trans_text = translation.text
-        tts = gTTS(trans_text, lang=output_language, tld=tld, slow=False)
+        #tts = gTTS(trans_text, lang=output_language, tld=tld, slow=False)
         try:
             my_file_name = text[0:20]
         except:
             my_file_name = "audio"
-        tts.save(f"temp/{my_file_name}.mp3")
+        #tts.save(f"temp/{my_file_name}.mp3")
         return my_file_name, trans_text
 
 
