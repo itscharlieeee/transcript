a904dd48853cb253757111fe7a2e1fe49d15b548
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-05-29 16:36:12 -0500

    Update video_trad.py

--- pages/video_trad.py
+++ pages/video_trad.py
@@ -91,23 +91,6 @@ def video_to_transcript(video_file):
 
 
     
-display_output_text = st.checkbox("Mostrar el texto")
-if st.button("convertir"):
-        result, output_text = text_to_speech(input_language, output_language, text, tld)
-        audio_file = open(f"temp/{result}.mp3", "rb")
-        audio_bytes = audio_file.read()
-        st.markdown(f"## Tú audio:")
-        st.audio(audio_bytes, format="audio/mp3", start_time=0)
-    
-        if display_output_text:
-            st.markdown(f"## Texto de salida:")
-            st.write(f" {output_text}")
-
-
-    
-    # Close the video and audio clips
-    audio_clip.close()
-    
 
 
 st.title("Transcripción de Video")
@@ -134,3 +117,20 @@ if video_file is not None:
 if st.button("Transcribir", type="primary"):
     video_to_transcript(video_file)
    
+display_output_text = st.checkbox("Mostrar el texto")
+if st.button("convertir"):
+        result, output_text = text_to_speech(input_language, output_language, text, tld)
+        audio_file = open(f"temp/{result}.mp3", "rb")
+        audio_bytes = audio_file.read()
+        st.markdown(f"## Tú audio:")
+        st.audio(audio_bytes, format="audio/mp3", start_time=0)
+    
+        if display_output_text:
+            st.markdown(f"## Texto de salida:")
+            st.write(f" {output_text}")
+
+
+    
+    # Close the video and audio clips
+        audio_clip.close()
+    
