1e9583f3f077c2b07448dc14b32b38d3daf30ed9
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-06-02 16:48:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,39 @@ if uploaded_file is not None:
     )
     st.write(transcription.text)
 
+if st.button("Traducir con IA",key=3):
+       in_lang= "English"   
+       out_lang="Spanish"
+       client = OpenAI(api_key=ke)  
+       response = client.chat.completions.create(
+         model="gpt-4o",
+         messages=[
+           {
+             "role": "system",
+             "content": f"You will be provided with a text in { in_lang }, and your task is to translate it into {out_lang}."
+           },
+           {
+             "role": "user",
+             "content": st.session_state.t_txt
+           }
+         ],
+         temperature=0.7,
+         max_tokens=300,
+         top_p=1
+       )
+       texto_traducido = response.choices[0].message.content
+       st.session_state.txt_tr=texto_traducido
+       st.subheader("Texto traducido:")
+       st.write(texto_traducido)
+if st.button("Audio traducido",key=4):
+  client = OpenAI(api_key=ke)
+  #speech_file_path = Path(__file__).parent / "speech.mp3"
+  response = client.audio.speech.create(
+    model="tts-1",
+    voice="alloy",
+    input=st.session_state.txt_tr
+  )
+  response.stream_to_file("trad_text.mp3")
+  st.audio("trad_text.mp3", format="audio/mp3") 
+
           
